f20bccb430100f9a99c615d24874afd794e71c8a
Lloyd Rees <lloyd@dreamatic.digital>
2025-08-07 22:59:33 +1000

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { CouponRedemptionForm } from "./components/CouponRedemptionForm";
 
 export default function App() {
   return (
-    <div className="bg-[#d8dff0] relative size-full" data-name="Wrapper">
+    <div className="bg-[#d8dff0] relative size-full min-h-screen" data-name="Wrapper">
       <CouponRedemptionForm />
     </div>
   );
